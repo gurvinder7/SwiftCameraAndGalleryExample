a989e5325293dd0cf2d82468a479eaa439172ee0
gurvinder mangat <gurvindermangat@gurvinders-MacBook-Pro.local>
2019-03-13 19:12:01 -0400

    create camera and gallery button

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgPhoto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btngalaryClick(_ sender: UIBarButtonItem) {
+        
+    }
+    @IBAction func btnCamaraClick(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
 
